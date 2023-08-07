01afb80aca09c5014d7673e46d7a4bc762b680c6
soutovp <fernandoandradesouto@hotmail.com>
2023-08-07 09:48:05 -0300

    update: Atualização do sistema de Conferência Físico

--- js/conferenciaFisico.js
+++ js/conferenciaFisico.js
@@ -1,70 +1,185 @@
+// let dados = [];
+// const imprimir = ()=>{
+//     const contagemText = document.getElementById('contagemText');
+//     contagemText.style.display = 'none';
+//     window.print();
+//     contagemText.style.display = 'flex';
+// }
+// const limpar = ()=>{
+//     const contagem = document.getElementById('contagem')
+//     contagem.value = "";
+//     dados.splice(0, dados.length);
+// }
+// const limparTabela =()=>{
+//     const table = document.getElementById('table')
+//     table.innerHTML ="<tr>"+
+//                     "<th>Referência</th>"+
+//                     "<th>Número</th>"+
+//                     "<th>Qtd. Físico</th>"+
+//                     "</tr>"
+// }
+// const separarTextoPorLinha = (texto)=>{
+//     let data = texto.split(['\n'])
+//     for(var i = 0; i<data.length; i++){
+//         data[i] = data[i].split(/[.;]/g)
+//     }
+//     return data;
+// }
+// const coletarDados = ()=>{
+//     const contagem = document.getElementById('contagem')
+//     dados = (separarTextoPorLinha(encontrarPalavrasRepetidas(contagem.value)))
+//     console.log(dados)
+//     constructTable();
+//     console.log('entrada : \n'+contagem.value)
+//     console.log('saída: \n'+encontrarPalavrasRepetidas(contagem.value))
+// }
+// const constructTable = ()=>{
+//     const table = document.getElementById('table')
+//     const ljOrigem = document.getElementById('ljOrigem')
+//     let data = ""
+//     for(var i = 0; i<dados.length; i++){
+//         console.log(dados[i][1])
+//         data +=`<tr><th>${dados[i][0]}</th><th>${dados[i].length === 2 ? "" : dados[i][1]}</th><th>${dados[i].length === 2 ? dados[i][1].toString() : dados[i][2]}</th></tr>`
+//     }
+//     table.innerHTML += data;
+// }
+// function encontrarPalavrasRepetidas(str) {
+//     const words = str.split(/\s+/); // Divide a string em palavras usando espaços em branco como separador
+//     const wordCount = {};
+
+//     // Conta a quantidade de vezes que cada palavra aparece
+//     words.forEach((word) => {
+//       const [palavra, quantidadeStr] = word.split(";");
+//       const quantidade = parseInt(quantidadeStr, 10) || 1;
+//       wordCount[palavra] = (wordCount[palavra] || 0) + quantidade;
+//     });
+
+//     const palavrasUnicas = {};
+
+//     // Concatena cada palavra única com sua quantidade e armazena no objeto
+//     words.forEach((word) => {
+//       const [palavra, quantidadeStr] = word.split(";");
+//       if (wordCount[palavra] > 1 && !palavrasUnicas[palavra]) {
+//         palavrasUnicas[palavra] = `${palavra};${wordCount[palavra]}`;
+//       } else if (!palavrasUnicas[palavra]) {
+//         palavrasUnicas[palavra] = `${palavra};1`;
+//       }
+//     });
+
+//     return Object.values(palavrasUnicas).join("\n");
+//   }
+
 let dados = [];
-const imprimir = ()=>{
-    const contagemText = document.getElementById('contagemText');
-    contagemText.style.display = 'none';
-    window.print();
-    contagemText.style.display = 'flex';
+const imprimir = () => {
+	const contagemText = document.getElementById('contagemText');
+	contagemText.style.display = 'none';
+	window.print();
+	contagemText.style.display = 'flex';
 }
-const limpar = ()=>{
-    const contagem = document.getElementById('contagem')
-    contagem.value = "";
-    dados.splice(0, dados.length);
+const limpar = () => {
+	const contagem = document.getElementById('contagem');
+	contagem.value = "";
+	dados.splice(0, dados.length);
 }
-const limparTabela =()=>{
-    const table = document.getElementById('table')
-    table.innerHTML ="<tr>"+
-                    "<th>Referência</th>"+
-                    "<th>Número</th>"+
-                    "<th>Qtd. Físico</th>"+
-                    "</tr>"
+const limparTabela = () => {
+	const table = document.getElementById('table');
+	table.innerHTML = "<tr>" +
+		"<th>Referência</th>" +
+		"<th>Qtd. Físico</th>" +
+		"<th>N33</th>" +
+		"<th>N34</th>" +
+		"<th>N35</th>" +
+		"<th>N36</th>" +
+		"<th>N37</th>" +
+		"<th>N38</th>" +
+		"<th>N39</th>" +
+		"<th>N40</th>" +
+		"<th>N41</th>" +
+		"<th>N42</th>" +
+		"<th>N43</th>" +
+		"<th>N44</th>" +
+		"<th>N45</th>" +
+		"<th>N46</th>" +
+		"</tr>";
 }
-const separarTextoPorLinha = (texto)=>{
-    let data = texto.split(['\n'])
-    for(var i = 0; i<data.length; i++){
-        data[i] = data[i].split(/[.;]/g)
-    }
-    return data;
+const separarTextoPorLinha = (texto) => {
+	let data = texto.split(['\n']);
+	for (var i = 0; i < data.length; i++) {
+		data[i] = data[i].split(/[.;]/g);
+	}
+	return data;
 }
-const coletarDados = ()=>{
-    const contagem = document.getElementById('contagem')
-    dados = (separarTextoPorLinha(encontrarPalavrasRepetidas(contagem.value)))
-    console.log(dados)
-    constructTable();
-    console.log('entrada : \n'+contagem.value)
-    console.log('saída: \n'+encontrarPalavrasRepetidas(contagem.value))
+const coletarDados = () => {
+	const contagem = document.getElementById('contagem');
+	dados = (separarTextoPorLinha(encontrarPalavrasRepetidas(contagem.value)));
+	console.log(dados);
+	constructTable();
+	console.log('entrada : \n' + contagem.value);
+	console.log('saída: \n' + encontrarPalavrasRepetidas(contagem.value));
 }
-const constructTable = ()=>{
-    const table = document.getElementById('table')
-    const ljOrigem = document.getElementById('ljOrigem')
-    let data = ""
-    for(var i = 0; i<dados.length; i++){
-        console.log(dados[i][1])
-        data +=`<tr><th>${dados[i][0]}</th><th>${dados[i].length === 2 ? "" : dados[i][1]}</th><th>${dados[i].length === 2 ? dados[i][1].toString() : dados[i][2]}</th></tr>`
-    }
-    table.innerHTML += data;
+const constructTable = () => {
+	const table = document.getElementById('table');
+	const ljOrigem = document.getElementById('ljOrigem');
+	let referencias = {};
+
+	for (var i = 0; i < dados.length; i++) {
+		const referencia = dados[i][0];
+		const numero = dados[i][1];
+		const quantidade = parseInt(dados[i][2]);
+
+		if (!referencias[referencia]) {
+			referencias[referencia] = {
+				quantidadeTotal: quantidade,
+				numeros: {
+					N33: 0, N34: 0, N35: 0, N36: 0, N37: 0,
+					N38: 0, N39: 0, N40: 0, N41: 0, N42: 0,
+					N43: 0, N44: 0, N45: 0, N46: 0
+				}
+			};
+		} else {
+			referencias[referencia].quantidadeTotal += quantidade;
+		}
+
+		if (numero in referencias[referencia].numeros) {
+			referencias[referencia].numeros[numero] += quantidade;
+		}
+	}
+
+	let data = "";
+	for (const referencia in referencias) {
+		data += `<tr><th>${referencia}</th><th>${referencias[referencia].quantidadeTotal}</th>`;
+		for (const numero in referencias[referencia].numeros) {
+			data += `<th ${referencias[referencia].numeros[numero] > 0 ? 'style="color:white; background-color: black"' : ""}>${referencias[referencia].numeros[numero]}</th>`;
+		}
+		data += "</tr>";
+	}
+
+	table.innerHTML += data;
 }
+
+// Resto do código permanece o mesmo
 function encontrarPalavrasRepetidas(str) {
-    const words = str.split(/\s+/); // Divide a string em palavras usando espaços em branco como separador
-    const wordCount = {};
-  
-    // Conta a quantidade de vezes que cada palavra aparece
-    words.forEach((word) => {
-      const [palavra, quantidadeStr] = word.split(";");
-      const quantidade = parseInt(quantidadeStr, 10) || 1;
-      wordCount[palavra] = (wordCount[palavra] || 0) + quantidade;
-    });
-  
-    const palavrasUnicas = {};
-  
-    // Concatena cada palavra única com sua quantidade e armazena no objeto
-    words.forEach((word) => {
-      const [palavra, quantidadeStr] = word.split(";");
-      if (wordCount[palavra] > 1 && !palavrasUnicas[palavra]) {
-        palavrasUnicas[palavra] = `${palavra};${wordCount[palavra]}`;
-      } else if (!palavrasUnicas[palavra]) {
-        palavrasUnicas[palavra] = `${palavra};1`;
-      }
-    });
-  
-    return Object.values(palavrasUnicas).join("\n");
-  }
+	const words = str.split(/\s+/); // Divide a string em palavras usando espaços em branco como separador
+	const wordCount = {};
+
+	// Conta a quantidade de vezes que cada palavra aparece
+	words.forEach((word) => {
+		const [palavra, quantidadeStr] = word.split(";");
+		const quantidade = parseInt(quantidadeStr, 10) || 1;
+		wordCount[palavra] = (wordCount[palavra] || 0) + quantidade;
+	});
+
+	const palavrasUnicas = {};
+
+	// Concatena cada palavra única com sua quantidade e armazena no objeto
+	words.forEach((word) => {
+		const [palavra, quantidadeStr] = word.split(";");
+		if (wordCount[palavra] > 1 && !palavrasUnicas[palavra]) {
+			palavrasUnicas[palavra] = `${palavra};${wordCount[palavra]}`;
+		} else if (!palavrasUnicas[palavra]) {
+			palavrasUnicas[palavra] = `${palavra};1`;
+		}
+	});
+
+	return Object.values(palavrasUnicas).join("\n");
+}
